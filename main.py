bfdbfdca795d7ffaec3d192eaf6d8ecf7e8b1593
ravenusmc <mcuddy77@gmail.com>
2017-05-03 21:11:41 -0400

    worked on login page

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def login():
-    url = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=61d27504cb064eaf912be90e31803d5d"
-    r = requests.get(url)
-    response_dict = r.json()
-    image = response_dict['articles'][0]['urlToImage']
-    news_title = response_dict['articles'][0]['title']
-    url = response_dict['articles'][0]['url']
-    print(image)
-    name = "Mike Hope"
-    return render_template('index.html', title='Login Page')
+    
+    return render_template('login.html', title='Login Page')
 
 #This line will actually run the app.
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+# url = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=61d27504cb064eaf912be90e31803d5d"
+# r = requests.get(url)
+# response_dict = r.json()
+# image = response_dict['articles'][0]['urlToImage']
+# news_title = response_dict['articles'][0]['title']
+# url = response_dict['articles'][0]['url']
+# print(image)
+# name = "Mike Hope"
