f6a4eeb8ba4753f12d2f32d8d992f5c3bbd8f214
ravenusmc <mcuddy77@gmail.com>
2017-05-07 19:44:51 -0400

    added a game page

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from user import *
 #Setting up Flask
 app = Flask(__name__)
 
+#This function brings the user to the login page
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -29,6 +30,7 @@ def login():
             return redirect(url_for('sign_up'))
     return render_template('login.html', title='Login Page')
 
+#This function brings the user to the sign up page.
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -49,22 +51,30 @@ def sign_up():
         return redirect(url_for('index'))
     return render_template('sign_up.html', title='Sign Up Page')
 
-@app.route('/index', methods=['GET', 'POST'])
+#This function brings the user to the home/index page
+@app.route('/index')
 def index():
+    #This line will ensure that the user is logged in.
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    #Getting the username to use on the site.
+    name = session['username']
+    #Pulling the news articles from the API and saving it in a variable named url.
+    url = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=61d27504cb064eaf912be90e31803d5d"
+    r = requests.get(url)
+    response_dict = r.json()
+    articles = response_dict['articles']
+    return render_template('index.html', title='Home Page', username = name, articles = articles)
+
+#This function will display the news articles that deal with video game news.
+@app.route('/games')
+def games():
     #This line will ensure that the user is logged in.
     if 'username' not in session:
         return redirect(url_for('login'))
     name = session['username']
     url = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=61d27504cb064eaf912be90e31803d5d"
-    r = requests.get(url)
-    response_dict = r.json()
-    articles = response_dict['articles']
-    length = len(response_dict['articles'])
-    return render_template('index.html', title='Home Page', username = name, articles = articles)
-
-@app.route('/no_match', methods=['GET', 'POST'])
-def no_match():
-    return render_template('no_match.html', title='Home Page')
+    return render_template('game.html', title='Home Page', username = name, articles = articles)
 
 # set the secret key. keep this really secret:
 app.secret_key = 'n3A\xef(\xb0Cf^\xda\xf7\x97\xb1x\x8e\x94\xd5r\xe0\x11\x88\x1b\xb9'
@@ -72,7 +82,3 @@ app.secret_key = 'n3A\xef(\xb0Cf^\xda\xf7\x97\xb1x\x8e\x94\xd5r\xe0\x11\x88\x1b\
 #This line will actually run the app.
 if __name__ == '__main__':
     app.run(debug=True)
-
-# image = response_dict['articles'][0]['urlToImage']
-# news_title = response_dict['articles'][0]['title']
-# url = response_dict['articles'][0]['url']
